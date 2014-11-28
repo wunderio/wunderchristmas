90463546595a35a74980fc894e2d9c034a06164d
David Corbacho <davidcorbacho@gmail.com>
2014-11-28 15:11:21 +0200

    Adding FastClick

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -67,4 +67,11 @@
   Gator(document).on('click', '.trigger-overlay', toggleOverlay);
   Gator(document).on('click', '.overlay-close', toggleOverlay);
 
+  if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+      FastClick.attach(document.body);
+    }, false);
+  }
+
 })();
+

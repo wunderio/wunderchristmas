52c6ac69b9373a87a3a2cf39a0403e29c87ef5e9
Lewis Nyman <hello@lewisnyman.co.uk>
2014-11-28 20:05:35 +0000

    Add GA tracking to day open

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -63,6 +63,7 @@
     else if( !classie.has( overlay, 'close' ) ) {
       classie.add( overlay, 'open' );
       classie.add( container, 'overlay-open' );
+      _gaq.push(['_trackEvent', 'Calendar - Day', 'Open', overlaySelector]);
     }
   }
 
